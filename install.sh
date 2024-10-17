ced208910b51b79e010f0d7fa6233d181c4464d3
asd <asd@asd.com>
2024-10-18 00:51:46 +0300

    Visual studio code

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ sudo apt install flameshot
 sudo apt install gawk wget git diffstat unzip texinfo gcc build-essential chrpath socat cpio python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev pylint3 xterm python3-subunit mesa-common-dev zstd liblz4-tool
 sudo apt install cargo
 sudo apt install net-tools
+sudo snap install code
